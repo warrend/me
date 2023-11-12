25a8c00f834766c9ed9945d1e5b9888f633a8c5c
Daniel Warren <warrend555@gmail.com>
2023-11-12 17:37:01 -0500

    deploy configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  // site: 'https://warrend.github.io',
+  site: 'https://danielwarren.io',
+  // base: '/me',
+  integrations: [react()],
 });
